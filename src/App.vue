24aacaf4e3cc6e93b60fc19f7ed34c9d58175063
noxxxxxious <sean.fried@gmail.com>
2024-04-18 00:48:25 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<main>
+		<AppStage />
+	</main>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+<script setup lang="ts">
+	import { provide } from 'vue'
+	import AppStage from './components/AppStage.vue'
+	import kanji from './assets/indexed_kanji.json'
+	provide('kanji', kanji)
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/AppStage.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script setup lang='ts'>
+	import { inject } from 'vue'
+	import { buildLimitedKanjiSet, getRandomKanjiSet } from '@/utils/utils'
+
+	const kanji = inject('kanji')
+	const limitedKanji = buildLimitedKanjiSet(kanji, 14)
+	const selectedKanji = getRandomKanjiSet(limitedKanji)
+
+	console.log(selectedKanji)
+
+</script>
+
+<style scoped>
+	
+</style>
